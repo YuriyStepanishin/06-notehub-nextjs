4f20397d6611d946095c88f4967aec66e9901ec3
Yuriy Stepanishin <y.stepanishin@gmail.com>
2026-03-04 12:44:51 +0200

    add loading component

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -5,10 +5,10 @@ import NotesClient from "./Notes.client";
 export default async function NotesPage() {
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ["notes"],
-    queryFn: () => fetchNotes(1),
-  });
+ await queryClient.prefetchQuery({
+  queryKey: ["notes", 1, ""],
+  queryFn: () => fetchNotes(1, ""),
+});
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
